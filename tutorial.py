1455fb832d0ddc8b1460932f8cd9f3e4d126131b
Victor-LP <victor.lpitcella@gmail.com>
2025-10-27 17:09:44 -0300

    Tutorial 1

--- tutorial.py
+++ tutorial.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+import pygame
+pygame.init()
+window = pygame.display.set_mode((500, 400))
+pygame.display.set_caption("Hello World!")
+game = True
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+    window.fill((255, 255, 255))
+    pygame.display.update()
+pygame.quit()
